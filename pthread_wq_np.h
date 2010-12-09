70b9b0ed9ce0a1330cc99ec5d090fb514b1ad980
Charles <charles.duyk@gmail.com>
2010-12-09 18:00:22 -0500

    added BSD license to every file, small resturcturing to wq_test.c

--- pthread_wq_np.h
+++ pthread_wq_np.h
@@ -1,15 +1,39 @@
-//Charles Duyk
-//charles.duyk@gmail.com
-//John Stumpo
-//stump@jstump.com 
+/* Copyright (c) 2010 Charles Duyk and John Stumpo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the names of the authors nor the names of other contributors
+ *        may be used to endorse or promote products derived from this
+ *        software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef PTHREAD_WQ_NP_H
 #define PTHREAD_WQ_NP_H
 
 /* Work queue priority attributes. */
 #define	WORKQ_HIGH_PRIOQUEUE	0	/* Assign to high priority queue. */
-#define WORKQ_DEFAULT_PRIOQUEUE	2	/* Assign to default priority queue. */
-#define WORKQ_LOW_PRIOQUEUE		4	/* Assign to low priority queue. */
+#define WORKQ_DEFAULT_PRIOQUEUE	1	/* Assign to default priority queue. */
+#define WORKQ_LOW_PRIOQUEUE		2	/* Assign to low priority queue. */
 
 
 typedef struct pthread_workqueue_s pthread_workqueue_t;
