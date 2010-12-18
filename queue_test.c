9147c75263ba7c0eb4179ece0a2a2e0da25a1b6b
Charles Duyk <charles.duyk@gmail.com>
2010-12-18 15:46:28 -0500

    Modified queue_test.c to test two stage allocation (no valgrind on osx--could not ensure no leaks.)

--- queue_test.c
+++ queue_test.c
@@ -30,20 +30,43 @@
 #include "stdincludes.h"
 #include "dequeue.h"
 
+#define NUM_ITEMS 50
 
 int main(void) {
-	dequeue_t *dequeue = dequeue_new();
-	for(int i = 0; i < 50; i++) {
-		int *lols = malloc(sizeof(int));
-		*lols = i;
-		(i % 2) ? dequeue_append(dequeue, lols) : dequeue_push(dequeue, lols);
+	dequeue_t 	*dequeue 	= dequeue_new();
+	uint32_t 	size 		= 0;
+	int 		*val 		= NULL;
+	list_node_t	*node		= NULL;
+	//test autoboxing calls
+	printf("regular calls (autoboxing pointers)");
+	for(int i = 0; i < NUM_ITEMS; i++) {
+		val = malloc(sizeof(int));
+		*val = i;
+		(i % 2) ? dequeue_append(dequeue, val) : dequeue_push(dequeue, val);
 	}
-	uint32_t size;
-	int *val;
 	while((size = dequeue_size(dequeue)) > 0) {
-		val = (size % 2 ? dequeue_pop(dequeue) : dequeue_pop_tail(dequeue));
+		val = ((size % 2) ? dequeue_pop(dequeue) : dequeue_pop_tail(dequeue));
 		printf("size: %u val = %d\n", size, *val);
 		free(val);
+	}
+	val = dequeue_pop(dequeue);
+	printf("empty queue pop val = %p, size = %u\n", (void *) val, dequeue_size(dequeue));
+	val = dequeue_pop_tail(dequeue);
+	printf("empty queue pop tail val = %p, size = %u \n", (void *) val, dequeue_size(dequeue));
+	
+	//two stage allocation
+	printf("testing two stage allocation:\n");
+	for(int i = 0; i < 50; i++) {
+		val = malloc(sizeof(int));
+		list_node_t *node = dequeue_node();
+		*val = i;
+		(i % 2) ? dequeue_push_node(dequeue, node, val) : dequeue_append_node(dequeue, node, val);
+	}
+	while((size = dequeue_size(dequeue)) > 0) {
+		val = ((size % 2) ? dequeue_pop_tail_node(dequeue, &node) : dequeue_pop_node(dequeue, &node));
+		printf("size = %u val = %d\n", size, *val);
+		free(val);
+		dequeue_node_free(node);
 	}
 	dequeue_free(dequeue);
 	return EXIT_SUCCESS;
